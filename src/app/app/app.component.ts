c677c1b003ebe060a72174a0664176ca90cc4847
gxx <562606139@qq.com>
2017-09-11 14:58:01 +0800

    9.11-gxx

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -18,10 +18,19 @@ export class AppComponent implements OnInit{
           });
     }
 
-  submit(con, phone){
+  submit(type, con, phone){
     let alert = con.trim();
     let mobiles = phone.trim();
-    this.service.jiguangPush({alert,mobiles,pushType:1})
+    let opt = {
+      alert,
+      mobiles,
+      pushType:1
+    }
+    if(!type){
+      opt.pushType = 0;
+      delete opt.mobiles;
+    }
+    this.service.jiguangPush(opt)
           .subscribe(data => {
               console.log(data)
           });
